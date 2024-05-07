319b8e40e285f026007a0bcf604149b402b4d631
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-05-07 22:34:16 +0300

    remove header ol component

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HeaderOldComponent } from './header-old/header-old.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, HeaderOldComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
