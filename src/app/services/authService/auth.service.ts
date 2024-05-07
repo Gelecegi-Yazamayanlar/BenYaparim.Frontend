d745e703cead4563cbf4ddd3ae865e3d417bf727
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-05-07 22:37:25 +0300

    add  auth utilities

--- src/app/services/authService/auth.service.ts
+++ src/app/services/authService/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { jwtDecode } from 'jwt-decode';
 
@@ -16,20 +16,49 @@ export interface IUser {
 })
 export class AuthService {
   private baseUrl = 'http://localhost:5233/api';
+  private isLoggedInSubject = new BehaviorSubject<boolean>(
+    this.checkTokenPresence(),
+  );
+  private userProfileSubject = new BehaviorSubject<any>(this.decodeToken());
 
   constructor(private http: HttpClient) {}
 
   login(credentials: { email: string; password: string }): Observable<any> {
-    return this.http.post(`${this.baseUrl}/Auth/Login`, credentials).pipe(
-      tap((response: any) => {
-        localStorage.setItem('token', response.token);
-        const decodedToken = jwtDecode(response.token);
-        console.log('Decoded JWT:', decodedToken);
-      }),
-    );
+    return this.http
+      .post(`${this.baseUrl}/Auth/Login`, credentials)
+      .pipe(tap((response: any) => this.handleAuthentication(response.token)));
   }
 
   signup(user: IUser): Observable<IUser> {
-    return this.http.post<IUser>(`${this.baseUrl}/Auth/Signup`, user);
+    return this.http.post<IUser>(`${this.baseUrl}/Auth/Register`, user);
+  }
+
+  logout(): void {
+    localStorage.removeItem('token');
+    this.isLoggedInSubject.next(false);
+    this.userProfileSubject.next(null);
+  }
+
+  isLoggedIn(): Observable<boolean> {
+    return this.isLoggedInSubject.asObservable();
+  }
+
+  getUserProfile(): Observable<any> {
+    return this.userProfileSubject.asObservable();
+  }
+
+  handleAuthentication(token: string): void {
+    localStorage.setItem('token', token);
+    this.isLoggedInSubject.next(true);
+    this.userProfileSubject.next(jwtDecode(token));
+  }
+
+  checkTokenPresence(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
+  decodeToken(): any {
+    const token = localStorage.getItem('token');
+    return token ? jwtDecode(token) : null;
   }
 }
