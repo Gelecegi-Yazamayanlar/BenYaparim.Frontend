9383588f09f5cea10d6e4d55f9375168ce118061
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2024-05-12 22:04:04 +0300

    add route components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 export const routes: Routes = [
   {
@@ -18,6 +21,21 @@ export const routes: Routes = [
     path: 'login',
     component: LoginComponent,
     title: 'Login',
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    title: 'Profile',
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    title: 'Dashboard',
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent,
+    title: 'Settings',
   },
 ];
 
